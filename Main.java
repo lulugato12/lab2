a47430cbdc9b17004d63a1c09d3c4399dda31b5f
Lourdes Cajica <36458387+lulugato12@users.noreply.github.com>
2019-09-17 14:15:55 -0500

    Team

--- Main.java
+++ Main.java
@@ -2,16 +2,22 @@ import java.util.Scanner;
 public class Main{
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int a = scan.nextInt();
-		int b = scan.nextInt();
-		int c = 0;
+		int a = Integer.parseInt(scan.nextLine());
+		int cont = 0;
 
-		while(a <= b){
-			a = a*3;
-			b = b*2;
-			c++;
+		for(int i = 0; i < a; i++){
+			String b = scan.nextLine();
+			int c = 0;
+			for(int j = 0; j < b.length(); j++){
+				if(b.charAt(j) == '1'){
+					c++;
+				}
+			}
+			if(c >= 2){
+				cont++;
+			}
 		}
 
-		System.out.println(c);
+		System.out.println(cont);
 	}
 }
