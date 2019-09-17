49674dccdf7f7f1169dafcaa80189ef1408f83ac
Lourdes Cajica <36458387+lulugato12@users.noreply.github.com>
2019-09-17 13:53:00 -0500

    bearandbigbrother

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main{
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		int c = 0;
+
+		while(a <= b){
+			a = a*3;
+			b = b*2;
+			c++;
+		}
+
+		System.out.println(c);
+	}
+}
